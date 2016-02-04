eb4d3ea2fac7ec2db0b2a30523618288975176d4
Dan Caragea <dancaragea@gmail.com>
2016-02-04 13:20:52 +0200

    chore(demo): added measurement times in demo

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Link from '../lib/ReactResponsiveLink';
+import Link from '../src/ReactResponsiveLink.jsx';
 
 class App extends React.Component {
 
+	onEventStart() {
+		this.start = Date.now();
+	}
+
 	onClickTap(e) {
 		e.preventDefault();
-		this.refs.write.textContent = 'Link was clicked/tapped';
+		this.refs.write.textContent = `Link was ${e.type == 'touchend' ? 'tapped' : 'clicked'} with a response time of ${Date.now() - this.start}ms`;
 	}
 
 	onClick(e) {
 		e.preventDefault();
-		this.refs.write.textContent = 'The A tag was clicked';
+		this.refs.write.textContent = `The A tag was clicked with a response time of ${Date.now() - this.start}ms`;
 	}
+
+	start = 0;
 
 	render() {
 		return (
 			<div>
-				<div>
-					<Link onClickTap={this.onClickTap}>Click to see the responsive link in action</Link>
-				</div>
-				<div>
-					<a onClick={this.onClick}>Click to see the regular a tag in action</a>
-				</div>
+				<Link onTouchStart={::this.onEventStart} onMouseDown={::this.onEventStart} onClickTap={::this.onClickTap}>Click to see the responsive link in action</Link>
+				<br />
+				<br />
+				<br />
 				<div ref="write"></div>
+				<br />
+				<br />
+				<a href="#" onMouseDown={::this.onEventStart} onClick={::this.onClick}>Click to see the regular a tag in action</a>
+
 			</div>
 		);
 	}
